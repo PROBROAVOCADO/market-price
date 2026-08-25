987179ab8f6a1244d9b581bc1152ad390b3a6e57
Hsieh <mike96531@gmail.com>
2026-08-25 11:47:14 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * 改版時務必更新 CACHE 名稱，否則舊快取不會被淘汰。
  * 快取名稱用 probro-market- 前綴，與出貨通知的 probro-ship- 區隔。
  */
-const CACHE = 'probro-market-v1.8.2-r2';
+const CACHE = 'probro-market-v1.8.2-r3';
 
 const ASSETS = [
   './', './index.html', './crop-catalog.js', './app.js', './manifest.json',
